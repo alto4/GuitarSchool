1200e61d67184db35b0cb2ea123583831059e33f
Scott Alton <scottaltonmusic@gmail.com>
2021-12-28 18:26:26 -0500

    Add user model and registration logic.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,70 @@
 const express = require('express');
 const router = express.Router();
-
-// GET api/users
-// Get users
+const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const brcypt = require('bcryptjs');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs/dist/bcrypt');
+// POST api/users
+// Register user
 // Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required to register').not().isEmpty(),
+    check('email', 'Please enter a valid email address.').isEmail(),
+    check('password', 'Please enter a password that is at least 6 characters in length.').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    try {
+      // Verify email is unique
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({
+          errors: [
+            {
+              msg: `User already exists with the email ${email}. Please sign in or register with a unique email address.`,
+            },
+          ],
+        });
+      }
+
+      // Try to retrieve gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return JWT
+
+      res.send('User successfully registered.');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error.');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect to MongoDB
 connectDatabase();
 
+// Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Welcome to Guitar School'));
 
 // Define routes

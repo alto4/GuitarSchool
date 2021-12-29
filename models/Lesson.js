6a84e35f0b8ecdfd896dceaa030139c147971770
Scott Alton <scottaltonmusic@gmail.com>
2021-12-29 18:58:25 -0500

    Added lesson model.

---
+++ models/Lesson.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const LessonSchema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  students: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Lesson = mongoose.model('lesson', LessonSchema);

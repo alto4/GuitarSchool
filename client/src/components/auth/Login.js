afb9280fd701a03c0da27d72609bb71b3266c967
Scott Alton <scottaltonmusic@gmail.com>
2022-01-23 22:55:24 -0500

    Login and registration forms added.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, name, password, passwordConfirm } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log('Login submission triggered.');
+    const user = { email, password };
+
+    console.log('Success! Submit the following form data => ', formData);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='auth-container'>
+      <form className='auth-form'>
+        <h1>Login</h1>
+        <label htmlFor='email'>Email Address</label>
+        <input type='text' name='email' value={email} onChange={(e) => onChange(e)} placeholder='Email Address' />
+        <label htmlFor='password'>Password:</label>
+        <input type='password' name='password' value={password} onChange={(e) => onChange(e)} placeholder='Password' />
+        <button className='auth-button' onClick={(e) => onSubmit(e)}>
+          Login
+        </button>
+        <hr />
+        <p>
+          Don't have an account? <Link to='/register'>Register</Link>
+        </p>
+      </form>
     </div>
   );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const { email, name, password, passwordConfirm } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log('sub');
+
+    if (password !== passwordConfirm) {
+      console.error('Passwords do not match.');
+      return;
+    }
+
+    const newUser = { name, email, password };
+
+    console.log('Success! Submit the following form data => ', formData);
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className='auth-container'>
+      <form className='auth-form'>
+        <h1>Register</h1>
+        <label htmlFor='email'>Email Address</label>
+        <input type='text' name='email' value={email} onChange={(e) => onChange(e)} placeholder='Email Address' />
+        <label htmlFor='name'>Full Name:</label>
+        <input type='text' name='name' value={name} onChange={(e) => onChange(e)} placeholder='Name' />
+        <label htmlFor='password'>Password:</label>
+        <input type='password' name='password' value={password} onChange={(e) => onChange(e)} placeholder='Password' />
+        <label htmlFor='passwordConfirm'>Confirm Password:</label>
+        <input
+          type='password'
+          name='passwordConfirm'
+          value={passwordConfirm}
+          onChange={(e) => onChange(e)}
+          placeholder='ConfirmPassword'
+        />
+        <button className='auth-button' onClick={(e) => onSubmit(e)}>
+          Register
+        </button>
+        <hr />
+        <p>
+          Already have an account? <Link to='/login'>Login</Link>
+        </p>
+      </form>
     </div>
   );
 };
